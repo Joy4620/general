e1ce4b266dad2af82a10c47c5419f0949ffc6506
Joy4620 <90296644+Joy4620@users.noreply.github.com>
2021-11-07 14:26:02 -0800

    Update floodfill class.py

--- floodfill class.py
+++ floodfill class.py
@@ -20,37 +20,13 @@ visited = ['w', 'A']
 current = 0
 
 while stack:
-    P = stack.pop(0)
+    P = stack.pop()
+    print(Matrix[P.x][P.y])
     if Matrix[P.x][P.y] not in visited:
         Matrix[P.x][P.y] = str(current)
         visited.append(str(current))
         current += 1
-        stack.extend([(P.x-1, P.y), (P.x+1, P.y), (P.x, P.y-1), (P.x, P.y+1)])
+        stack.extend([Floodfill(P.x-1, P.y), Floodfill(P.x+1, P.y), Floodfill(P.x, P.y-1), Floodfill(P.x, P.y+1)])
+        # stack.extend([object1，object2，object3, object4])
 for i in Matrix:
     print(i)
-
-
-# print(node.x)
-# P = stack[0]
-# print(type(P.x))
-# print(Matrix[P.x][P.y] in 'S')
-# print(P.x-1)
-# class Dog:
-#     def __init__(self, name):
-#         self.name = name
-#         self.steps = 0
-#     def bark(self):
-#         print(self.name, 'bark!')
-
-# hehe = Dog('hrfud')
-# hehe.bark()
-
-# Stack = [(4, 1)]
-# while Stack:
-#     x, y = Stack.pop(0)
-#     if Matrix[x][y] not in 'w0A':
-#         Matrix[x][y] = '0'
-#         Stack.append((x-1, y))
-#         Stack.append((x+1, y))
-#         Stack.append((x, y+1))
-#         Stack.append((x, y-1))
