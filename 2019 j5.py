2297956655d2d6767dcd57a1e24ac5562c4e0db8
Joy4620 <90296644+Joy4620@users.noreply.github.com>
2021-12-07 20:31:03 -0800

    Add files via upload

--- 2019 j5.py
+++ 2019 j5.py
@@ -10,6 +10,12 @@
     3 3 AAAA
     1 3 AAAB
 
+    Alternative Output:
+    2 1 BB
+    3 2 BAA
+    1 2 BAB
+    3 1 AAAB
+
     input details
     - first 3 lines are sub. rules
     - 4th line: s - # of steps, 
@@ -19,8 +25,22 @@
     - p - position of starting index
     - w - sequence that results'''
 
-rulekey = ['AA', 'AB', 'B']
-rulevalue = ['AB', 'B', 'AB']
+rulekey = []
+rulevalue = []
+
+for i in range(3):
+    rules = input().split()
+    rulekey.append(rules[0])
+    rulevalue.append(rules[1])
+
+s, i, f = input().split()
+
+# rulekey = ['AA', 'AB', 'B']
+# rulevalue = ['AB', 'BB', 'AA']
+
+# AA - AB - BB - AAB - AAAA
+
+# s, i, f = ['4', 'AA', 'AAAA']
 
 class Variation():
     def __init__(self, dstr, location = None, rule = None, parent=None):
@@ -34,9 +54,9 @@ class Variation():
             self.record=parent.record+self.record
 
 
-StartVariation = Variation('AB')
-Stack = [StartVariation]
+Stack = [Variation(i)]
 visited=[]
+
 
 def replace(rulenumber, string):
     output = []
@@ -45,28 +65,28 @@ def replace(rulenumber, string):
         a = string.find(rulekey[rulenumber], i)
         if a != -1 and a != previous:
             previous = a
-            output.append([string[:i] + string[i:].replace(rulekey[rulenumber], rulevalue[rulenumber], 1), a + 1, rulenumber])
+            output.append([string[:i] + string[i:].replace(rulekey[rulenumber], rulevalue[rulenumber], 1), a, rulenumber])
     return output
 
 
 while Stack:
     P = Stack.pop()
-    print(P.record)
     if P.dstr in visited:
         continue
-    if P.step == 4:
-        if P.dstr == 'AAAB':
-            print(P.record)
+    if P.step == int(s):
+        if P.dstr == str(f):
+            for i in P.record[1:]:
+                print(f'{i[0]+1} {i[1]+1} {i[2]}')
             break
         continue
     visited.append(P.dstr)
     for i in range(3):
         a = replace(i, str(P.dstr))
-        for i in a:
-            Stack.append(Variation(i[0], i[1], i[2], P))
+        for j in a:
+            Stack.append(Variation(j[0], j[1], j[2], P))
 
 
-print(P.step, 'p.step')
-print(P.record, 'p.record')
-print(visited, 'visited')
+# print(P.step, 'p.step')
+# print(P.record, 'p.record')
+# print(visited, 'visited')
 
